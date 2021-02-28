ba1a5e18fda90b0f0e883b21d926605ec55a3a58
A manoi <amanoi@sina.com>
2021-02-28 17:53:02 +0800

    typescript_ls

--- wx/miniprogram/pages/index/index.ts
+++ wx/miniprogram/pages/index/index.ts
@@ -7,7 +7,7 @@ Page({
     motto: 'Hello World',
     userInfo: {},
     hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    // canIUse: wx.canIUse('button.open-type.getUserInfo'),
   },
   // 事件处理函数
   bindViewTap() {
@@ -16,33 +16,15 @@ Page({
     })
   },
   onLoad() {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true,
-      })
-    } else if (this.data.canIUse) {
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
+app.globalData.userInfo.then(
+      userInfo=>{
         this.setData({
-          userInfo: res.userInfo,
+          userInfo:userInfo,
           hasUserInfo: true,
         })
       }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true,
-          })
-        },
-      })
-    }
-    this.updateMotto()
+    )
+    //this.updateMotto()
   },
   getUserInfo(e: any) {
     console.log(e)
