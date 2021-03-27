3c54b93be3ad8d0a7a9f21b41d65416e7abd02ff
A manoi <amanoi@sina.com>
2021-03-27 20:47:02 +0800

    完成高标准表驱动测试模版

--- server/auth/dao/mongo.go
+++ server/auth/dao/mongo.go
@@ -4,9 +4,10 @@ import (
 	"context"
 	"fmt"
 
-	"coolcar/share/mongo/mgo"
+	mgo "coolcar/share/mongo"
 
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
@@ -15,21 +16,25 @@ const opedIDField = "open_id"
 
 // Mongo defines a mongo dao.
 type Mongo struct {
-	col *mongo.Collection
+	col      *mongo.Collection
+	newObjID func() primitive.ObjectID
 }
 
 // NewMongo creates a new mongo dao.
 func NewMongo(db *mongo.Database) *Mongo {
 	return &Mongo{
-		col: db.Collection("account"),
+		col:      db.Collection("account"),
+		newObjID: primitive.NewObjectID,
 	}
 }
 
 // ResolveAccountID reslove an account id from open id.
 func (m *Mongo) ResolveAccountID(c context.Context, openID string) (string, error) {
+	insertedID := m.newObjID()
 	res := m.col.FindOneAndUpdate(c, bson.M{
 		opedIDField: openID,
-	}, mgo.Set(bson.M{
+	}, mgo.SetOnInsert(bson.M{
+		mgo.IDField: insertedID,
 		opedIDField: openID,
 	}), options.
 		FindOneAndUpdate().
--- server/auth/dao/mongo_test.go
+++ server/auth/dao/mongo_test.go
@@ -2,26 +2,94 @@ package dao
 
 import (
 	"context"
+	"os"
 	"testing"
 
+	mgo "coolcar/share/mongo"
+	mongotesting "coolcar/share/mongo/testing"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
+var mongoURI string
+
 func TestResolveAccountID(t *testing.T) {
 	c := context.Background()
-	mc, err := mongo.Connect(c, options.Client().ApplyURI("mongodb://localhost:55044/?readPreference=primary&ssl=false"))
+	mc, err := mongo.Connect(c, options.Client().ApplyURI(mongoURI))
 	if err != nil {
 		t.Fatalf("cannot connect mongodb:%v", err)
 	}
 	m := NewMongo(mc.Database("coolcar"))
-	id, err := m.ResolveAccountID(c, "123")
+	_, err = m.col.InsertMany(c, []interface{}{
+		bson.M{
+			mgo.IDField: mustObjID("605d838cbcfcb14576815cbc"),
+			opedIDField: "openid_1",
+		},
+		bson.M{
+			mgo.IDField: mustObjID("605d838cbcfcb14576915cbe"),
+			opedIDField: "openid_2",
+		},
+	})
 	if err != nil {
-		t.Fatalf("fail resolve account id for 123: %v", err)
-	} else {
-		want := "605d838cbcfcb14576c15cb3"
-		if id != want {
-			t.Errorf("resolve account id want: %q,got:%q", want, id)
-		}
+		t.Fatalf("cannot insert inital values: %v", err)
 	}
+	m.newObjID = func() primitive.ObjectID {
+		return mustObjID("605d838cbcfcb14576c15cb4")
+	}
+
+	cases := []struct {
+		name   string
+		openID string
+		want   string
+	}{
+		{
+			name:   "existing_user",
+			openID: "openid_1",
+			want:   "605d838cbcfcb14576815cbc",
+		},
+		{
+			name:   "another_existing_user",
+			openID: "openid_2",
+			want:   "605d838cbcfcb14576915cbe",
+		},
+		{
+			name:   "new_user",
+			openID: "openid_3",
+			want:   "605d838cbcfcb14576c15cb4",
+		},
+	}
+	for _, cc := range cases {
+		t.Run(cc.name, func(t *testing.T) {
+			id, err := m.ResolveAccountID(context.Background(), cc.openID)
+			if err != nil {
+				t.Errorf("fail resolve account id for %q:%v\n", cc.openID, err)
+			}
+			if id != cc.want {
+				t.Errorf("resolve account id want: %q,got:%q", cc.want, id)
+			}
+		})
+	}
+	// id, err := m.ResolveAccountID(c, "123")
+	// if err != nil {
+	// 	t.Fatalf("fail resolve account id for 123: %v", err)
+	// } else {
+	// 	want := "605d838cbcfcb14576c15cb3"
+	// 	if id != want {
+	// 		t.Errorf("resolve account id want: %q,got:%q", want, id)
+	// 	}
+	// }
+}
+func mustObjID(hex string) primitive.ObjectID {
+	objID, err := primitive.ObjectIDFromHex(hex)
+	if err != nil {
+		panic(err)
+	}
+	return objID
+}
+
+func TestMain(m *testing.M) {
+	os.Exit(mongotesting.RunWithMongoInDocker(m, &mongoURI))
 }
